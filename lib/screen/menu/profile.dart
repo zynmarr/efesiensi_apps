94f63d2a7fc932c2b890bbfaf8a6181d131d1cb3
Munawir712 <munawirjr2@gmail.com>
2020-08-08 21:31:10 +0700

    edit profile

--- lib/screen/menu/profile.dart
+++ lib/screen/menu/profile.dart
@@ -36,7 +36,9 @@ class _ProfileState extends State<Profile> {
                               Icons.edit,
                               color: Colors.white,
                             ),
-                            onPressed: null),
+                            onPressed: () {
+                              _editProfile();
+                            }),
                         IconButton(
                             icon: Icon(
                               Icons.settings,
@@ -118,6 +120,196 @@ class _ProfileState extends State<Profile> {
     );
   }
 
+  Future _editProfile() {
+    return showModalBottomSheet(
+        isScrollControlled: true,
+        backgroundColor: Colors.grey[300],
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        )),
+        isDismissible: false,
+        context: context,
+        builder: (context) {
+          return Container(
+            height: MediaQuery.of(context).size.height / 1.2,
+            child: ListView(
+              physics: ScrollPhysics(),
+              // crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    width: 100,
+                    height: 5,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  child: Text(
+                    "Edit Profile",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Container(
+                    margin: EdgeInsets.symmetric(
+                      horizontal: 10.0,
+                    ),
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          _nama(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          _email(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          _phone(),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          _asal(),
+                          SizedBox(
+                            height: 150,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(30.0)),
+                                  padding: const EdgeInsets.all(0.0),
+                                  onPressed: () {},
+                                  color: Colors.grey[400],
+                                  child: Center(
+                                    child: Text(
+                                      "batal".toUpperCase(),
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: MediaQuery.of(context)
+                                                .textScaleFactor +
+                                            18,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 50,
+                                width: 150,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50)),
+                                child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(30.0)),
+                                  padding: const EdgeInsets.all(0.0),
+                                  onPressed: () {},
+                                  color: Colors.transparent,
+                                  child: Ink(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.all(
+                                        Radius.circular(50.0),
+                                      ),
+                                      gradient: LinearGradient(
+                                        colors: [
+                                          Color(0xFFff6f00),
+                                          Color(0xFFff6d00),
+                                          Color(0xFFff9800),
+                                        ],
+                                        begin: Alignment.centerRight,
+                                        end: Alignment.centerLeft,
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        "Simpan".toUpperCase(),
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: MediaQuery.of(context)
+                                                  .textScaleFactor +
+                                              18,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          );
+        });
+  }
 
-  
+  Widget _nama() {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.white),
+        child: TextFormField(
+          decoration: InputDecoration(
+              hintText: "Nama",
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.person)),
+        ));
+  }
+
+  Widget _email() {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.white),
+        child: TextFormField(
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              hintText: 'Email',
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.mail)),
+        ));
+  }
+
+  Widget _phone() {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.white),
+        child: TextFormField(
+          keyboardType: TextInputType.phone,
+          decoration: InputDecoration(
+              hintText: 'No Hp',
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.phone)),
+        ));
+  }
+
+  Widget _asal() {
+    return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), color: Colors.white),
+        child: TextFormField(
+          decoration: InputDecoration(
+              hintText: 'Asal',
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.location_on)),
+        ));
+  }
 }
